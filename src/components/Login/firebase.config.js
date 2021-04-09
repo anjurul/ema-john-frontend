61a63a617f2534b243d346487ccf0ed659dc1999
anjurul <anjurul.ewallet@gmail.com>
2021-04-10 00:17:33 +0400

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCDvDjPZaWB4ysBK6wMbUBxmRve9L5sE2s",
+  authDomain: "ema-zon-simple-auth.firebaseapp.com",
+  projectId: "ema-zon-simple-auth",
+  storageBucket: "ema-zon-simple-auth.appspot.com",
+  messagingSenderId: "820751675217",
+  appId: "1:820751675217:web:e5772fdbce5cc4a823ad70"
+};
+  export default firebaseConfig;
